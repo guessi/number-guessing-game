066bf53545a3e71f8e3bbe7f87b3524659eb1f9a
guessi <guessi@gmail.com>
2024-01-05 14:12:18 +0800

    Keep sample code aligned with rust-lang doc

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number from 1 to 100.");
+    println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     loop {
+        println!("Please input your guess.");
+
         let mut guess = String::new();
 
         io::stdin()
@@ -19,11 +21,13 @@ fn main() {
             Err(_) => continue,
         };
 
+        println!("You guessed: {guess}");
+
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("Bingo, congrats!");
+                println!("You win!");
                 break;
             }
         }
